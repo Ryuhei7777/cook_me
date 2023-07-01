a8bcbd35a785d6b1172dd1126008e8ef69b04e51
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-07-01 14:02:27 +0000

    [in_recipe_detail_implementation]

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe < ApplicationRecord
   has_many :seasonings, inverse_of: :recipe
   has_many :tags, inverse_of: :recipe
   belongs_to :customer
-  
+
   accepts_nested_attributes_for :materials, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :seasonings, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: :all_blank
