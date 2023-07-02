5879be3eb97d80fad143df36dde6b1ee928634ff
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-07-02 05:03:25 +0000

    [recipe_detail_implementation_complete]

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -5,10 +5,12 @@ class Public::BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new
     @bookmark.customer_id = current_customer.id
-    @bookmark.recipe_id = @recipe
+    @bookmark.recipe_id = params[:recipe_id]
     @bookmark.save
+    redirect_to recipe_path(@bookmark.recipe_id)
   end
 
   def destroy
   end
+
 end
--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -15,6 +15,8 @@ class Public::RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @comment = Comment.new
+    @comments = Comment.where(recipe_id: @recipe).page(params[:page]).per(5)
+
   end
 
   def edit
