412746c38615a220aa2b54f6d0ab652c600bbe4f
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-07-05 09:55:54 +0000

    [bookmark_implementation_complete]

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class Public::BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.where(customer_id: current_customer.id).page(params[:page]).per(8)
   end
 
   def create
@@ -11,6 +12,9 @@ class Public::BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path
   end
 
 end
