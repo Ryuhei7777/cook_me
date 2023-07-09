c3c9bd76ffaf53e090505c7da9b5a7ff89ccd9bb
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-07-09 10:48:06 +0000

    [in_admin_member_detail_implementation]

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RecipesController < ApplicationController
   def show
+     @recipe = Recipe.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.where(recipe_id: @recipe).page(params[:page]).per(5)
   end
 end
