11968bbecc595bf4c0fbd6031bb4a1ef7ec53e5f
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-07-14 22:53:58 +0000

    [in_admin_top_screen_implementation]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,5 +13,15 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :is_deleted)
+  end
+
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @recipes = Recipe.all.order(created_at: :desc).page(params[:page]).per(8)
   end
 end
